50b5cf561b6f607850fe501e7904ff8a56f53bbe
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2024-07-19 21:33:47 +0530

    Create Day6_ProductArrayPuzzle.java

---
+++ Day6_ProductArrayPuzzle.java
@@ -0,0 +1,28 @@
+class Solution 
+{ 
+	public static long[] productExceptSelf(int nums[], int n) 
+	{ 
+        // code here
+        long[] leftProduct = new long[n];
+        long[] rightProduct = new long[n];
+        
+        long[] result = new long[n];
+        
+        leftProduct[0] = 1;
+        rightProduct[n-1] = 1;
+        
+        for(int i = 1; i<n; i++){
+            leftProduct[i] = leftProduct[i-1] * nums[i-1]; 
+        }
+        
+        for(int i = n-2; i>=0; i--){
+            rightProduct[i] = rightProduct[i+1] * nums[i+1];
+        }
+        
+        for(int i = 0; i<n; i++){
+            result[i] = leftProduct[i] * rightProduct[i];
+        }
+        
+        return result;
+	} 
+} 
