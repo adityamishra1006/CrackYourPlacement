1a0df6f90987bad891f641663ffce978c614eccc
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2024-07-19 23:19:40 +0530

    Create Day6_RemoveDuplicatesFromSortedArray.java

---
+++ Day6_RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head==null || head.next == null) return head;
+        ListNode temp = head;
+        while(temp.next!=null){
+            if(temp.val == temp.next.val){
+                temp.next = temp.next.next;
+            }
+            else{
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
+}
