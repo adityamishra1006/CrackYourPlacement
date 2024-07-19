5fabd473dd880bbec5a75438847de1f6e2c69947
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2024-07-19 22:59:53 +0530

    Create Day6_FindPeakelement.java

---
+++ Day6_FindPeakelement.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+
+        int lo = 0;
+        int hi = n-1;
+
+        while(lo<hi){
+            int mid = lo + (hi - lo)/2;
+            for(int i = 0; i<n; i++){
+                if(nums[mid] > nums[mid+1]) hi = mid;
+                else lo = mid+1;
+            }
+        }
+        return lo;
+    }
+}
