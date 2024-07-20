c29e7033903c9a43b5b677f3d9d316318b453e23
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2024-07-20 23:50:48 +0530

    Create Day7_PathSum.java

---
+++ Day7_PathSum.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null) return false;
+        if(root.left == null && root.right == null){
+            return targetSum == root.val;
+        }
+
+        targetSum -= root.val;
+
+        return (hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum));
+    }
+}
