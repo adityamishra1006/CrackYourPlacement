bb8f04df9213444dc5ac4221fb012580baf2a8a0
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2024-07-24 21:40:42 +0530

    Create Day11_AddTwonumberII.java

---
+++ Day11_AddTwonumberII.java
@@ -0,0 +1,42 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
+        
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        int carry = 0;
+        
+        while (l1 != null || l2 != null || carry != 0) {
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+            
+            int total = val1 + val2 + carry;
+            carry = total / 10;
+            int digit = total % 10;
+            
+            current.next = new ListNode(digit);
+            current = current.next;
+            
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        
+        // Reverse the result and return
+        return reverseList(dummy.next);
+    }
+    
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        
+        return prev;
+    }
+}
