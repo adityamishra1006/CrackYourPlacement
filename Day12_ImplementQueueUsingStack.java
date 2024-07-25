33cd66ccc8cb497628e7c02e303ee960ff5aab57
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2024-07-25 22:26:44 +0530

    Create Day12_ImplementQueueUsingStack.java

---
+++ Day12_ImplementQueueUsingStack.java
@@ -0,0 +1,33 @@
+class MyQueue {
+
+    Stack<Integer> input;
+    Stack<Integer> output;
+
+    public MyQueue() {
+        input = new Stack<>();
+        output = new Stack<>();
+    }
+    
+    public void push(int x) {
+        input.push(x);
+    }
+    
+    public int pop() {
+        peek();
+        return output.pop();
+    }
+    
+    public int peek() {
+        if(output.empty()){
+            while(!input.empty()){
+                output.push(input.pop());
+            }
+        }
+        return output.peek();
+    }
+    
+    public boolean empty() {
+        return input.empty() && output.empty();
+    }
+}
+
