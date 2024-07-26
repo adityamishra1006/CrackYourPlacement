df7c15ea4828594e4ba2f208d8eff0d4139c9a0c
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2024-07-26 22:16:24 +0530

    Create Day13__DeleteNthNodeFromEndOfList.java

---
+++ Day13__DeleteNthNodeFromEndOfList.java
@@ -0,0 +1,18 @@
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        for(int i =1; i<=n; i++){
+            fast = fast.next;
+        }
+        if(fast==null) return head.next;
+
+        while(fast.next!=null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
