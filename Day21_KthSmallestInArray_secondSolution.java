36cbf7370c9e33a02902a46e2e4bb8a62f53adeb
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2024-08-03 15:55:47 +0530

    Update Day21_KthSmallestInArray_secondSolution.java

--- Day21_KthSmallestInArray_secondSolution.java
+++ Day21_KthSmallestInArray_secondSolution.java
@@ -1,8 +1,6 @@
-class Solution{
-    public static int kthSmallest(int[] arr, int l, int r, int k) 
-    { 
-        //Your code here
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+class Solution {
+    public int findKthLargest(int[] arr, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         int n = arr.length;
         for(int i = 0; i<n; i++){
             pq.add(arr[i]);
@@ -12,5 +10,5 @@ class Solution{
             pq.remove();
         }
         return pq.peek();
-    } 
+    }
 }
