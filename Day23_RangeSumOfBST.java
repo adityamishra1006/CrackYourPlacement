508329a7314aed18dcf309b70f1ea30c7fe44892
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2024-08-05 22:44:37 +0530

    Create Day23_RangeSumOfBST.java

---
+++ Day23_RangeSumOfBST.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int rangeSumBST(TreeNode root, int lo, int hi) {
+        if(root==null) return 0;
+        int sum = rangeSumBST(root.left, lo, hi)+rangeSumBST(root.right, lo, hi);
+        if(root.val >= lo && root.val<=hi) sum+=root.val;
+        return sum;
+    }
+}
