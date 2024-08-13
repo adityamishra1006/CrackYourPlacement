65a5d5c75650909794799ca8ad2eca92eaad5daa
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2024-08-13 22:20:22 +0530

    Create Day31_MinimumMovesToEqualArrayElement.java

---
+++ Day31_MinimumMovesToEqualArrayElement.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+
+        int median = nums[nums.length/2];
+
+        int moves = 0;
+
+        for(int num : nums){
+            moves += Math.abs(median - num);
+        }
+
+        return moves;
+    }
+}
